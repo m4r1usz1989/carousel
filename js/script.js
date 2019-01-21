e16a558cf31b1429b3ad9b4b4214f994d2d2b42d
Mariusz Domanski <mariusz@domanski.pl>
2019-01-21 21:02:38 +0100

    Creating a slider

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  pageDots: false,
+  hash: true,
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+
+});
+
+var progressBar = document.querySelector('.progress-bar')
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
