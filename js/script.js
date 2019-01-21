655d4687cd5b1dda85242b56c25352ab5559d40b
Mariusz Domanski <mariusz@domanski.pl>
2019-01-21 21:42:35 +0100

    Add restart button

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ var flkty = new Flickity( elem, {
   hash: true,
 });
 
-
-// element argument can be a selector string
-//   for an individual element
-
-
 var progressBar = document.querySelector('.progress-bar')
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
+});
+
+var restart = document.getElementById('restart');
+restart.addEventListener('click', function (){
+	flkty.select(0);
 });
