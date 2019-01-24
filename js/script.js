0f76b089e6e8049d56137cc462072807c0d6911e
Mariusz Domanski <mariusz@domanski.pl>
2019-01-24 22:03:30 +0100

    Modified script.js

--- js/script.js
+++ js/script.js
@@ -1,56 +1,51 @@
 'use strict';
 (function(){ 
+	// mustache
+	var slide = document.getElementById('slide').innerHTML;
+	var mainCarousel = document.querySelector('.main-carousel');
+	var slideItem = '';
 
-// mustache
+	Mustache.parse(slide);
+	for(var i = 0; i < data.length; i++) {
+		slideItem += Mustache.render(slide, data[i]);
+	}
 
-var slide = document.getElementById('slide').innerHTML;
-var mainCarousel = document.querySelector('.main-carousel');
-var slideItem = '';
+	mainCarousel.insertAdjacentHTML('beforeend', slideItem);
 
-Mustache.parse(slide);
+	var elem = document.querySelector('.main-carousel');
+	var flkty = new Flickity( elem, {
+		// options
+		cellAlign: 'left',
+		contain: true,
+		pageDots: false,
+		hash: true,
+	});
 
-for (var i = 0; i < data.length; i++) {
-	slideItem += Mustache.render(slide, data[i]);
-}
+	var progressBar = document.querySelector('.progress-bar')
+	flkty.on( 'scroll', function( progress ) {
+		progress = Math.max( 0, Math.min( 1, progress ) );
+		progressBar.style.width = progress * 100 + '%';
+	});
 
-mainCarousel.insertAdjacentHTML('beforeend', slideItem);
-
-
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  pageDots: false,
-  hash: true,
-});
-
-var progressBar = document.querySelector('.progress-bar')
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
-});
-
-var restart = document.getElementById('restart');
-restart.addEventListener('click', function (){
-	flkty.select(0);
-});
-
+	var restart = document.getElementById('restart');
+	restart.addEventListener('click', function (){
+		flkty.select(0);
+	});
+	
 })();
 
 // google map
-	window.initMap = function() {
-		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 6,
-			center: data[0].coords
+window.initMap = function() {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 6,
+		center: data[0].coords
+	});
+
+	var markers = [];
+	for(var i = 0; i < data.length; i++) {
+		markers[i] = new google.maps.Marker({
+			position: data[i].coords,
+			map: map
 		});
-
-		var markers = [];
-
-		for(var i = 0; i < data.length; i++) {
-			markers[i] = new google.maps.Marker({
-				position: data[i].coords,
-				map: map
-			});
-		}
 	}
+}
